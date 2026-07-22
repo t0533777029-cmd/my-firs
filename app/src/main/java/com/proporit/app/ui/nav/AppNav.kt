168f403d74fff9d4f1619067410b0351f4cea61f
t0533777029-cmd <t0533777029@gmail.com>
2026-07-23 01:09:16 +0300

    первая версия

--- app/src/main/java/com/proporit/app/ui/nav/AppNav.kt
+++ app/src/main/java/com/proporit/app/ui/nav/AppNav.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.unit.dp
